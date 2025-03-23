5b37a2bcf586b39f291a1e2539c0a0166f0177b1
VitorLugon <103381252+VitorLugon@users.noreply.github.com>
2025-03-23 18:35:11 -0300

    Update Program.cs

--- Quetao1/ProgramacaoDistribuidaEConcorrente/QuestaoUm/Program.cs
+++ Quetao1/ProgramacaoDistribuidaEConcorrente/QuestaoUm/Program.cs
@@ -7,8 +7,10 @@ Conta conta1 = new(1, "João", 50);
 Thread gastadora = new Thread(() => new Gastadora(conta1).Run().Wait());
 Thread esperta = new Thread(() => new Esperta(conta1).Run().Wait());
 Thread economica = new Thread(() => new Economica(conta1).Run().Wait());
+Thread patrocinadora = new Thread(() => new Patrocinadora(conta1).Run().Wait());
 
 // Iniciando as threads
 esperta.Start();
 gastadora.Start();
 economica.Start();
+patrocinadora.Start();
